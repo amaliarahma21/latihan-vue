7cdc307c054153d76d24e6608d1562f5ec8cb2e7
amaliarahma21 <21221753100@smkassalaambandung.sch.id>
2023-07-27 14:23:58 +0700

    mengubah gallery latihan-vue

--- src/SatuComponent.vue
+++ src/SatuComponent.vue
@@ -1,23 +1,27 @@
 <template>
+    <div class="container mx-auto">
 <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+    
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://images.tokopedia.net/img/cache/700/VqbcmM/2022/3/26/485fbe83-1505-4e39-b611-9df2baad54f8.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="https://images.tokopedia.net/img/cache/700/VqbcmM/2022/3/26/485fbe83-1505-4e39-b611-9df2baad54f8.jpg" alt=""> 
+    </div>
+
+    <div>
+        <img class="h-auto max-w-full rounded-lg" src="https://id-test-11.slatic.net/p/94108987f87a73ffe35c4ec59c406074.jpg" alt="" margin="10px">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://id-test-11.slatic.net/p/94108987f87a73ffe35c4ec59c406074.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="https://cf.shopee.co.id/file/id-11134207-7qula-lgtifjh6btc35d" alt="" margin="10px">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://cf.shopee.co.id/file/id-11134207-7qula-lgtifjh6btc35d" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="https://images.tokopedia.net/img/cache/700/VqbcmM/2021/8/13/4f6e27b4-d700-4b8c-af25-c37881782ded.jpg" alt="" margin="10px">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://images.tokopedia.net/img/cache/700/VqbcmM/2021/8/13/4f6e27b4-d700-4b8c-af25-c37881782ded.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="https://down-id.img.susercontent.com/file/sg-11134201-23030-dp8weqd0t6nvc5" alt="" margin="10px">
     </div>
     <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://id-test-11.slatic.net/p/29e2044467a2bef4b53f3811df9c9068.jpg" alt="">
+        <img class="h-auto max-w-full rounded-lg" src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//83/MTA-24031342/no-brand_no-brand_full04.jpg" alt="" margin="10px">
     </div>
-    <div>
-        <img class="h-auto max-w-full rounded-lg" src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//83/MTA-24031342/no-brand_no-brand_full04.jpg" alt="">
-    </div>
+</div>
     
 </div>
 </template>
